5de334632f2ab7faa9de6114d7f1935482b750fa
Arpit Trikha <arpit.trikha@gmail.com>
2021-02-04 13:40:23 +0530

    Fetch Handling Errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -13,8 +13,22 @@ export const fetchDishes = () => (dispatch) => {
 
     dispatch(dishesLoading(true));
     return fetch(baseUrl+'dishes')
+    .then(response=>{
+        if(response.ok)
+        return response
+        else{
+            var error=new Error('Error'+response.status+':'+response.statusText)
+            error.response=response;
+            throw error;
+        }
+    },
+    error=>{
+        var errmess=new Error(error.message)
+        throw errmess
+    })
     .then(response=>response.json())
     .then(dishes=>dispatch(addDishes(dishes)))
+    .catch(error=>dispatch(dishesFailed(error.message)))
     
 }
 
@@ -28,8 +42,22 @@ export const dishesFailed = (errmess) => ({
 });
 export const fetchComments=()=>(dispatch)=>{
     return fetch(baseUrl+'comments')
+    .then(response=>{
+        if(response.ok)
+        return response
+        else{
+            var error=new Error('Error'+response.status+':'+response.statusText)
+            error.response=response;
+            throw error;
+        }
+    },
+    error=>{
+        var errmess=new Error(error.message)
+        throw errmess
+    })
     .then(response=>response.json())
     .then(comments=>dispatch(addComments(comments)))
+    .catch(error=>dispatch(commentsFailed(error.message)))
 }
 export const commentsFailed=(errmess)=>({
 type:ActionTypes.COMMENTS_FAILED,
@@ -42,8 +70,22 @@ export const addComments=(comments)=>({
 export const fetchPromos=()=>(dispatch)=>{
     dispatch(promosLoading())
     return fetch(baseUrl+'promotions')
+    .then(response=>{
+        if(response.ok)
+        return response
+        else{
+            var error=new Error('Error'+response.status+':'+response.statusText)
+            error.response=response;
+            throw error;
+        }
+    },
+    error=>{
+        var errmess=new Error(error.message)
+        throw errmess
+    })
     .then(response=>response.json())
     .then(promos=>dispatch(addPromos(promos)))
+    .catch(error=>dispatch(promosFailed(error.message)))
 }
 export const promosLoading=()=>({
     type:ActionTypes.PROMOS_LOADING
